3e257fc6de1f2ba8b74e785216b072a4dccce211
Documenter.jl <documenter@juliadocs.github.io>
2022-12-22 14:59:16 +0000

    build based on 7470f53

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = WaveSpec","category":"page"},{"location":"#WaveSpec","page":"Home","title":"WaveSpec","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for WaveSpec.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [WaveSpec]","category":"page"}]
+}
